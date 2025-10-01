44e9d8c376efc0a175c661bebb3e6fe3d52504be
Rhenn3D <aalvarez@ctsformacion.com>
2025-10-01 11:27:30 +0200

    NoAnimacion

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,15 +1,19 @@
 using System.Collections;
-using System.Threading;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneLoader : MonoBehaviour
 {
     public static SceneLoader Instance;
     [SerializeField] private GameObject _loadingCanvas;
+    [SerializeField] private Image _loadingBar;
+    private Animator _animator;
+
 
     void Awake()
     {
+        _animator = GetComponent<Animator>();
         if (Instance != null && Instance != this)
         {
             Destroy(gameObject);
@@ -33,8 +37,23 @@ public class SceneLoader : MonoBehaviour
 
         AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
 
+        asyncLoad.allowSceneActivation = false;
+
+        float fakeLoadPercentage = 0f;
+
+
         while (!asyncLoad.isDone)
         {
+            //_loadingBar.fillAmount = asyncLoad.progress;
+            _animator.SetBool("IsLoading", true);
+
+            fakeLoadPercentage += 0.001f;
+            _loadingBar.fillAmount = fakeLoadPercentage;
+
+            if (asyncLoad.progress >= 0.9f && fakeLoadPercentage >= 0.99f)
+            {
+                asyncLoad.allowSceneActivation = true;
+            }
             yield return null;
         }
 
